eb3b23bc4ebd1ae01495587a75ae651066205fd8
Vili Huotari <vili.huotari@gmail.com>
2023-09-23 13:47:26 +0300

    Amount selector added to tip section

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,17 +11,31 @@ struct ContentView: View {
     @State private var checkamount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPrecent = 20
+    @State private var tipAmount = 0
+    @State private var tipDefault = "Percentage"
     @FocusState private var amountIsFocused: Bool
+
     
-    let tipPrecentages = [10, 15, 20, 25, 0]
+    let tip = ["Amount", "Percentage"]
+    var totalAmount: Double {
+        var tipSelection = 0.0
+        var tipValue = 0.0
+        if (tipDefault == "Percentage"){
+            tipSelection = Double(tipPrecent)
+            tipValue = checkamount / 100 * tipSelection
+        }else {
+            tipValue = Double(tipAmount)
+        }
+        
+        let grandTotal = checkamount + tipValue
+        
+        return grandTotal
+    }
     
     var totalperPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPrecent)
-        
-        let tipValue = checkamount / 100 * tipSelection
-        let grandTotal = checkamount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+
+        let amountPerPerson = totalAmount / peopleCount
         
         return amountPerPerson
     }
@@ -36,25 +50,49 @@ struct ContentView: View {
                             .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<10){
+                        ForEach(2..<11){
                             Text("\($0) people")
                         }
                     }
                 }
                 
                 Section{
-                    Picker("Tip percentage", selection: $tipPrecent) {
-                        ForEach(tipPrecentages, id: \.self) {
-                            Text($0, format: .percent)
+                    Picker("", selection: $tipDefault) {
+                        ForEach(tip, id: \.self) {
+                            Text($0)
                         }
                     }
                     .pickerStyle(.segmented)
+                    if (tipDefault == "Percentage"){
+                        Picker("", selection: $tipPrecent) {
+                            ForEach(0..<101, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .padding(.trailing, 125)
+                    }else {
+                        Picker("", selection: $tipAmount) {
+                            ForEach(0..<Int(checkamount + 1), id: \.self) {
+                                Text($0, format: .currency(code: Locale.current.currency?.identifier ?? "EURO"))
+                            }
+                        }
+                        .padding(.trailing, 125)
+                    }
+
                 }header: {
-                    Text("Tip percentage")
+                    Text("Tip")
+                }
+                
+                Section{
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EURO"))
+                }header: {
+                    Text("Total amount")
                 }
                 
                 Section{
                     Text(totalperPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EURO"))
+                }header: {
+                    Text("Amout per person")
                 }
             }
             .navigationTitle("WeSplit")
